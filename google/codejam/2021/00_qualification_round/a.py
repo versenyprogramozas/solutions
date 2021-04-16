71bc9adb9dc21fdeb289a1c5f7dbfe747bb3e7da
Viktória Nemkin <viktoria.nemkin@gmail.com>
2021-04-16 23:55:57 +0200

    Create a.py

---
+++ google/codejam/2021/00_qualification_round/a.py
@@ -0,0 +1,11 @@
+n = int(input())
+for n_i in range(n):
+  cost = 0
+  m = int(input())
+  l = list(map(int, input().split(' ')))
+  for i in range(m-1):
+    j = l.index(min(l[i:m]))
+    cost += j-i+1
+    l[i:j+1] = reversed(l[i:j+1])
+  print(f"Case #{n_i+1}: {cost}")
+  
