cd3da5dd8cd161b8e79408e50712f408c11a42b2
Viktoria Nemkin <viktoria.nemkin@gmail.com>
2022-09-17 00:22:20 +0200

    Add CF1721

---
+++ codeforces/1721/c.py
@@ -0,0 +1,23 @@
+from bisect import bisect_left  # std::lower_bound
+cases = int(input())
+
+for _ in range(cases):
+
+  n = int(input())
+
+  a = list(map(int, input().split(" ")))
+  b = list(map(int, input().split(" ")))
+
+  # Minimumok
+  print(" ".join(
+      map(lambda i: str(b[bisect_left(b, a[i], lo=0, hi=i+1)]-a[i]), range(n))))
+
+  # Maximumok
+  mx = [0]*n
+  k = 0
+  for i in range(n):
+    k = max(k, i)  # Don't restart search
+    while k < n-1 and b[k] >= a[k+1]:
+      k += 1
+    mx[i] = b[k]-a[i]
+  print(" ".join(map(str, mx)))
